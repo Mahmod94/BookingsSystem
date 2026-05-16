17a3d8ff9a1efa28b75ad3470a4cc48f693940ac
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-05-16 11:48:08 +0200

    1. Added dependency Jpa 2. created interface BookingRepository 3. implement empty and all args constructors for Booking.java 4. implemented postRoom and getAll in BookingController.java

--- src/main/java/com/hashim/BookingsSystem/controller/RoomController.java
+++ src/main/java/com/hashim/BookingsSystem/controller/RoomController.java
@@ -1,10 +1,31 @@
 package com.hashim.BookingsSystem.controller;
 
+import com.hashim.BookingsSystem.model.Booking;
+import com.hashim.BookingsSystem.repository.BookingRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
 public class RoomController {
+    private BookingRepository bookingRepository;
 
+    public RoomController(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
+    @GetMapping("/booking")
+    public List<Booking> getAll()
+    {
+        return bookingRepository.findAll();
+    }
+
+    @PostMapping("/booking")
+    public String postBooking(@RequestParam Booking booking){
+        bookingRepository.save(booking);
+        return booking.toString();
+    }
 }
--- src/main/java/com/hashim/BookingsSystem/model/Booking.java
+++ src/main/java/com/hashim/BookingsSystem/model/Booking.java
@@ -1,11 +1,36 @@
 package com.hashim.BookingsSystem.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Booking {
-
-
+    @Id
+    @GeneratedValue
     private Integer id;
+
     private String guestName;
     private String roomType;
     private Integer numberOfGuests;
     private Double totalPrice;
+
+
+    public Booking(Integer id, String guestName, String roomType, Integer numberOfGuests, Double totalPrice) {
+        this.id = id;
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.numberOfGuests = numberOfGuests;
+        this.totalPrice = totalPrice;
+    }
+
+    public Booking() {
+    }
+
+    @Override
+    public String toString() {
+        return "Id: " + id + "\n" + "Guest Name: " + guestName + "\n"
+                + "roomType: " + roomType + "\nNumber of guests: " + numberOfGuests
+                 + "\nTotal price: " + totalPrice;
+    }
 }
