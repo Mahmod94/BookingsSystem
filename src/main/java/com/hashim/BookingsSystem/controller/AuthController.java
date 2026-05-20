55f12f68b8449327b6db0288197ce8024055c023
Mahmod94 <78415546+Mahmod94@users.noreply.github.com>
2026-05-20 08:44:44 +0200

    Implemented JwtService.java

--- src/main/java/com/hashim/BookingsSystem/controller/AuthController.java
+++ src/main/java/com/hashim/BookingsSystem/controller/AuthController.java
@@ -1,9 +1,15 @@
 package com.hashim.BookingsSystem.controller;
 
+import com.hashim.BookingsSystem.service.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthController {
 
+    private AuthenticationManager authManager;
+
+    private JwtService jwtService;
 
 }
--- src/main/java/com/hashim/BookingsSystem/service/JwtService.java
+++ src/main/java/com/hashim/BookingsSystem/service/JwtService.java
@@ -1,7 +1,31 @@
 package com.hashim.BookingsSystem.service;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
 
 @Service
 public class JwtService {
+    private final String secret = "I've_heard_he_spends_500_pounds_on_jeans_goes_to_the_gym_6_times_a_week_get_tatoos_he_" +
+            "doesn't_even_know_what_it_means_but_i_heard_he_makes_you_happy_so_its_fine_by_my";
+    private final Key key = Keys.hmacShaKeyFor(secret.getBytes());
+
+    public String generateToken (String username) {
+        return Jwts.builder().setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 2 )) // 2 h
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String validateTokenAndGetUserName(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .build().parseSignedClaims(token)
+                .getBody().getSubject();
+    }
 }
